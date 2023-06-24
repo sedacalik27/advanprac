651d1ae42610c41f296cce20f6979fc528921383
seda calik <okyanusdalevent@gmail.com>
2023-06-24 20:06:23 +0300

    guncel

---
+++ src/main/java/practice06/s02_oop_project/Aslan.java
@@ -0,0 +1,4 @@
+package practice06.s02_oop_project;
+
+public class Aslan {
+}
---
+++ src/main/java/practice06/s02_oop_project/Hayvan.java
@@ -0,0 +1,17 @@
+package practice06.s02_oop_project;
+
+public abstract class Hayvan {
+   private String ad;
+   private int yas;
+   private String tur;
+    //bu classı abstract yapmaya karar verdik
+    //pojo class:variable+getter+setter+constructor iceren depo classlar
+
+    public abstract void yemekYer();
+
+    public abstract void sesCikarir();
+
+
+
+
+}
